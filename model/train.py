5161a3093440db32c5166b7405d1df9c21040c02
InkiInki <1964158105@qq.com>
2024-08-13 16:14:16 +0800

    Add train.py of DKMIL

--- model/train.py
+++ model/train.py
@@ -1,73 +1,160 @@
 import torch
-from args.args_dkmil import lambda_l1, epoch
-from sklearn.metrics import roc_auc_score
-
-torch.set_default_tensor_type('torch.FloatTensor')
-
-
-def get_acc(model, B, y, device):
-    score = model(B.to(device))[0]
-    y_hat = torch.ge(score, 0.5).float()
-    acc = y_hat.eq(y).cpu().float().mean().item()
-
-    return acc, y_hat
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from args.args_basic import device, TORCH_SEED, os
+from utils.func_basic import write2file
 
 
-def train(loader_tr, loader_te, model, loss_func, optimizer, device, trained=False):
-    """"""
-    batch_count = 0
-    max_recorder = [0, None, 0, 0, -1]
+def get_model2D(args, round_idx=0, knowledge=None, model_save_home=r"D:\Data\Pretrain\Model\OOD"):
+    os.makedirs(model_save_home, exist_ok=True)
+    save_path = os.path.join(model_save_home,
+                             f"{args.data_type}{args.target_number}_{args.net_type}_seed{TORCH_SEED}_round{round_idx}"
+                             f".pth")
+    if args.net_type == "camil":
+        save_path = os.path.join(r"D:\Data\Pretrain\Model\CaMIL",
+                                 f"{args.data_type}{args.target_number}_{args.net_type}_"
+                                 f"abmil_seed{TORCH_SEED}_round{round_idx}.pth")
+    trainer, d, num_channel = None, -1, 3
+    extractor = "cnn"
+    if args.data_type == "mnist":
+        d = 50 * 4 * 4 if args.net_type != "mamil" else 48 * 5 * 5
+        num_channel = 1
+    elif args.data_type == "cifar10":
+        d = 50 * 5 * 5 if args.net_type != "mamil" else 48 * 6 * 6
+        # extractor = "resnet"
+    elif args.data_type == "stl10":
+        d = 50 * 21 * 21 if args.net_type != "mamil" else 48 * 22 * 22
+    elif args.data_type == "tumor":
+        d = 50 * 125 * 125 if args.net_type != "mamil" else 50 * 126 * 126
+    elif args.data_type == "imagenet":
+        d = 50 * 72 * 72 if args.net_type != "ma" else 48 * 73 * 73
 
-    for i in range(epoch):
-        # print_progress_bar(i, epoch)
-        tr_loss, tr_acc, te_acc = 0, 0, 0
+    trainer = Trainer(args, d=d, num_channel=num_channel, extractor=extractor, knowledge=knowledge)
 
-        model.train()
-        for (B, y) in loader_tr:
-            y_prob = model(B.to(device))[0]
+    return trainer, save_path
+
+
+def get_model(args, knowledge, loop, fold_idx,
+              model_save_home=r"D:\Data\Pretrain\Model\Benchmark"):
+    os.makedirs(model_save_home, exist_ok=True)
+    save_path = os.path.join(model_save_home,
+                             f"{args.data_type}_{args.net_type}_seed{TORCH_SEED}_loop{loop}_fold{fold_idx}.pth")
+
+    trainer = Trainer(args, d=knowledge.d, extractor="none", knowledge=knowledge)
+
+    return trainer, save_path
+
+
+class Trainer:
+    def __init__(self, args, d=50*4*4, num_channel=1, knowledge=None, extractor="cnn"):
+
+        if extractor != 'none':
+            from model.dkmil2D import DKMIL
+            self.net = DKMIL(args, d, knowledge, in_channels=num_channel, extractor=extractor)
+            self.target_number = args.target_number
+        else:
+            from model.dkmil import DKMIL
+            self.net = DKMIL(args, d, knowledge)
+        self.args = args
+        self.extractor = extractor
+        self.net.to(device)
+        self.best_net = None
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(filter(lambda p: p.requires_grad, self.net.parameters()),
+                                    lr=args.lr, weight_decay=args.weight_decay)
+        self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, args.epochs, 0.000005)
+
+        self.epochs = args.epochs
+        self.lambda_l1 = args.lambda_l1
+
+        self.metrics = {"acc": accuracy_score, "f1": f1_score, "auc": roc_auc_score}
+        self.record_save_path = os.path.join(r".\record" if extractor != "none" else r".\record\benchmark",
+                                             f"{args.data_type}_{args.net_type}_seed{TORCH_SEED}")
+        os.makedirs(self.record_save_path, exist_ok=True)
+
+    def training(self, tr_loader, te_loader, model_save_path, round_idx, loop=0):
+        if self.extractor != "none":
+            file_path = self.record_save_path + \
+                        "/target_number{:d}_round{:d}.txt".format(self.args.target_number, round_idx)
+        else:
+            file_path = self.record_save_path + \
+                        "/loop{:d}.txt".format(loop, round_idx)
+        best_acc, best_f1, best_auc = -1, -1, -1
+        if os.path.exists(model_save_path):
+            write2file(file_path, context="Loading pretrained model...\n")
+            self.net.load_state_dict(torch.load(model_save_path))
+            self.best_net = self.net
+            best_acc, best_f1, best_auc = self.testing(te_loader)
+            write2file(file_path, context="Best acc {:.4f}, f1 {:.4f}, auc {:.4f}\n"
+                       .format(best_acc, best_f1, best_auc))
+        else:
+            write2file(file_path, context="Training...\n")
+            for epoch in range(self.epochs):
+                total_loss = 0.0
+                self.net.train()
+                for batch_idx, (bag, y) in enumerate(tr_loader):
+                    # Get each bag with label
+                    if isinstance(y, list):
+                        y = y[0].float()
+                    # y = y[0].type(torch.LongTensor)
+                    bag, y = bag.to(device), y.to(device)
+
+                    self.optimizer.zero_grad()
+
+                    # Prediction and computing loss
+                    y_score = self.net(bag)[0]
+
+                    # loss = self.criterion(y_score, y.reshape(1, 1))
+                    loss = -1. * (y * torch.log(y_score) + (1. - y) * torch.log(1. - y_score))
+                    l1 = torch.tensor([0]).float().to(device)
+                    for param in self.net.parameters():
+                        l1 += torch.norm(param, 1)
+                    loss = loss + self.lambda_l1
+                    loss.sum().backward()
+                    self.optimizer.step()
+                    self.scheduler.step()
+                    total_loss += loss.cpu().float().mean().item()
+
+                self.net.eval()
+                acc1, f11, auc1 = self.testing(tr_loader)
+                acc2, f12, auc2 = self.testing(te_loader)
+
+                if epoch > 5 and best_acc < acc2:
+                    best_acc = acc2
+                    best_f1 = f12
+                    best_auc = auc2
+                    self.best_net = self.net
+                    torch.save(self.best_net.state_dict(), model_save_path)
+
+                write2file(file_path, context="Epoch {:5d}/{:5d}, training loss {:.4f}; "
+                                              "training acc {:.4f}, f1 {:.4f}, auc {:.4f}; "
+                                              "testing acc {:.4f}, f1 {:.4f}, auc {:.4f}; "
+                                              "best acc {:.4f}, f1 {:.4f}, auc {:.4f}\n"
+                           .format(epoch, self.epochs, total_loss,
+                                   acc1, f11, auc1, acc2, f12, auc2, best_acc, best_f1, best_auc))
+
+                if best_acc == 1.0:
+                    break
+        return self.best_net, best_acc, best_f1, best_auc, self.record_save_path
+
+    def testing(self, data_loader):
+        self.net.eval()
+        total_loss = 0.
+        y_list, y_hat_list = [], []
+        for batch_idx, (bag, y) in enumerate(data_loader):
             if isinstance(y, list):
                 y = y[0].float()
+            bag, y = bag.to(device), y.to(device)
+            y_score, y_hat, _ = self.net(bag)
+            y_list.append(y.reshape(1).detach().cpu().numpy().tolist()[0])
+            y_hat_list.append(y_hat.reshape(1).detach().cpu().numpy().tolist()[0])
 
-            # loss = -1. * (y * torch.log(y_prob) + (1. - y) * torch.log(1. - y_prob - 1e-6))
-            loss = loss_func(y_prob, y.reshape(1, 1))
-            l1 = torch.tensor([0]).float()
-            for param in model.parameters():
-                l1 += torch.norm(param, 1)
-            loss = loss + lambda_l1 * l1
-            # print(loss, y, y_prob)
-            optimizer.zero_grad()
-            loss.sum().backward()
-            optimizer.step()
-            tr_loss += loss.cpu().float().mean().item()
-            batch_count += 1
+            loss = self.criterion(y_score, y.reshape(1, 1))
+            total_loss += loss.cpu().float().mean().item()
 
-        model.eval()
-        for (B_ne, y_ne) in loader_tr:
-            if isinstance(y_ne, list):
-                y_ne = y_ne[0].float()
+        acc = self.metrics["acc"](y_list, y_hat_list)
+        f1 = self.metrics["f1"](y_list, y_hat_list)
+        auc = self.metrics["auc"](y_list, y_hat_list)
 
-            acc, _ = get_acc(model, B_ne, y_ne, device)
-            tr_acc += acc
-
-        Y, Y_hat = [], []
-        for (B_te, y_te) in loader_te:
-            if isinstance(y_te, list):
-                y_te = y_te[0].float()
-
-            acc, y_hat = get_acc(model, B_te, y_te, device)
-            y_te = y_te.reshape(1).detach().cpu().numpy().tolist()[0]
-            y_hat = y_hat.reshape(1).detach().cpu().numpy().tolist()[0]
-            Y.append(y_te)
-            Y_hat.append(y_hat)
-            te_acc += acc
-
-        tr_loss = tr_loss / len(loader_tr)
-        tr_acc = tr_acc / len(loader_tr)
-        te_acc = te_acc / len(loader_te)
-        auc = roc_auc_score(Y, Y_hat)
-        print("Epoch: %d, train loss: %.4f, train acc: %.4f, test acc: %.4f, auc: %.4f" %
-              (i, tr_loss, tr_acc, te_acc, auc))
-        if i >= 5 and te_acc > max_recorder[-2]:
-            max_recorder = [i, model, tr_acc, te_acc, auc]
-
-    return max_recorder
+        return acc, f1, auc
